ab0b890a2cf8d6af546541f5838f5ba8781b01a3
Orion Radford <omradford@gmail.com>
2022-02-04 11:46:10 -0600

    working on nltk stopwords not removing the correct words

--- 2mapRandom.py
+++ 2mapRandom.py
@@ -3,16 +3,14 @@
 
 import sys 
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 
 stopWords = set(stopwords.words('english'))
 
 # for each line, reverse
 for line in sys.stdin:
   strList = line.strip().split('\t')
-  if not strList in stopWords:
+  if strList[0] not in stopWords:
       if (len(strList) == 2):
         key, value = strList
-        # print(value,'\t',key)
         # right justify using 8 columns - sorting done automatically
         print(value.strip().zfill(8),'\t',key)
